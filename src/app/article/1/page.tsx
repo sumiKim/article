f27c2cdb5b0796ce2f9cbda53e2fde9cd6bfc5cd
sumiKim <charlette7@naver.com>
2023-12-27 17:59:56 +0900

    article 상단 Memo를 컴포넌트로 만듦

--- src/app/article/1/page.tsx
+++ src/app/article/1/page.tsx
@@ -1,43 +1,14 @@
 'use client';
+import ArticleMemo from '@/components/ArticleMemo';
 import './styles.css';
-import Link from 'next/link';
+
 import React, { useState } from 'react';
 export default function Article1Page() {
   const [isActive, setIsActive] = useState(false);
   const onClick = () => setIsActive(!isActive);
   return (
     <div>
-      <section className='memo_container p-2'>
-        <h1 className='font-bold border-b-2 border-black pb-1 mb-1'>
-          useState만 사용해서 만든 드롭다운
-        </h1>
-        <div className='memo flex flex-col gap-2'>
-          <div>
-            <p>&#91;CheckPoints&#93;</p>
-            <ul className='pl-5 list-inside list-decimal'>
-              <li>
-                user1 버튼 클릭시, menu 드롭다운되는 것. (useState로 on off
-                관리)
-              </li>
-              <li>
-                css styling : opacity + visibility + transform + transition
-                (visibility만 쓸 때보다 자연스러움)
-              </li>
-            </ul>
-          </div>
-          <div>
-            <p>&#91;ImprovePoints&#93;</p>
-            <ul className='pl-5 list-inside list-square'>
-              <li>menu 드롭다운후, 외부 영역 누르면 드롭다운 닫히기</li>
-              <li className='text-blue-500 hover:cursor-pointer hover:underline'>
-                <Link href='/article/2'>
-                  &#61;&#62; article 2) 외부 영역 클릭시, 드롭다운 닫히기
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </section>
+      <ArticleMemo article_id={1} />
       <section className='container'>
         <div className='menu-container'>
           <button onClick={onClick} className='menu-trigger'>
---
+++ src/components/ArticleMemo.tsx
@@ -0,0 +1,47 @@
+'use client';
+import { Article, getArticle } from '@/data/article';
+import Link from 'next/link';
+export default function ArticleMemo({ article_id }: { article_id: number }) {
+  const { id, title, date_created, check_points, improve_points }: Article =
+    getArticle(article_id);
+
+  return (
+    <section className='memo_container p-2'>
+      <h1 className='font-bold border-b-2 border-black pb-1 mb-1 flex justify-between'>
+        <span>
+          {id}) {title}
+        </span>
+        <span className='text-sm flex items-end'>{date_created}</span>
+      </h1>
+      <div className='memo flex flex-col gap-2'>
+        <div>
+          <p>&#91;CheckPoints&#93;</p>
+          <ul className='pl-5 list-inside list-decimal'>
+            {check_points.map((point, index) => (
+              <li key={`check_points_${index}`}>{point.content}</li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <p>&#91;ImprovePoints&#93;</p>
+          <ul className='pl-5 list-inside list-square'>
+            {improve_points.map((point, index) =>
+              point.type === 'text' ? (
+                <li key={`check_points_${index}`}>{point.content}</li>
+              ) : (
+                <li
+                  key={`improve_points_${index}`}
+                  className='text-blue-500 hover:cursor-pointer hover:underline'
+                >
+                  <Link href={point.hyperlink ?? ''}>
+                    &#61;&#62; {point.content}
+                  </Link>
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
